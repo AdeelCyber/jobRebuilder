d4d66a8e6f871b828b024a8a83aa95eca98c051b
Rayyanirfan2 <89404776+Rayyanirfan2@users.noreply.github.com>
2023-01-04 22:09:07 +0500

    Added new icons

---
+++ assets/Svgs/ArrowRightIcon.js
@@ -0,0 +1,13 @@
+const ArrowRightIcon = `<svg width="17" height="17" viewBox="0 0 17 17" fill="none" xmlns="http://www.w3.org/2000/svg">
+<g clip-path="url(#clip0_33_6416)">
+<path d="M5.22703 14.8821C5.57411 15.2292 6.1337 15.2292 6.48078 14.8821L12.367 8.99589C12.6433 8.71964 12.6433 8.27339 12.367 7.99714L6.48078 2.11089C6.1337 1.76381 5.57412 1.76381 5.22703 2.11089C4.87995 2.45798 4.87995 3.01756 5.22703 3.36464L10.3554 8.50006L5.21995 13.6355C4.87995 13.9755 4.87995 14.5421 5.22703 14.8821Z" fill="black"/>
+</g>
+<defs>
+<clipPath id="clip0_33_6416">
+<rect width="17" height="17" fill="white" transform="translate(17 17) rotate(-180)"/>
+</clipPath>
+</defs>
+</svg>
+`
+
+export default ArrowRightIcon
