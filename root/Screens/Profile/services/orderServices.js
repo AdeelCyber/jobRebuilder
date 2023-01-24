627cc56056da717dc9d3bee8336d04ce45ed1989
Rayyanirfan2 <89404776+Rayyanirfan2@users.noreply.github.com>
2023-01-24 23:15:32 +0500

    Added services

--- root/Screens/Profile/services/orderServices.js
+++ root/Screens/Profile/services/orderServices.js
@@ -15,3 +15,77 @@ export const getOrders = async () => {
     return error.response
   }
 }
+
+export const getOrderCategoryWise = async (category) => {
+  try {
+    const token = await AsyncStorage.getItem('@accessToken')
+    const config = {
+      header: {
+        Authorization: `Bearer ${token}`,
+      },
+      data: {
+        category,
+      },
+    }
+    const resp = axios.post('/orders/category', config)
+    return resp
+  } catch (error) {
+    return error.response
+  }
+}
+
+export const getSingleOrder = async (orderId) => {
+  try {
+    const token = await AsyncStorage.getItem('@accessToken')
+    const config = {
+      header: {
+        Authorization: `Bearer ${token}`,
+      },
+      data: {
+        orderId,
+      },
+    }
+    const resp = axios.post('/orders/order', config)
+    return resp
+  } catch (error) {
+    return error.response
+  }
+}
+
+export const deliverOneTimeOrder = async (orderId, comment, attachments) => {
+  try {
+    const token = await AsyncStorage.getItem('@accessToken')
+    const config = {
+      header: {
+        Authorization: `Bearer ${token}`,
+      },
+      data: {
+        orderId,
+        comment,
+        attachments,
+      },
+    }
+    const resp = await axios.put('/orders/oneTime/deliver', config)
+    return resp
+  } catch (error) {
+    return error.response
+  }
+}
+
+export const cancelOneTimeOrder = async (orderId) => {
+  try {
+    const token = await AsyncStorage.getItem('@accessToken')
+    const config = {
+      header: {
+        Authorization: `Bearer ${token}`,
+      },
+      data: {
+        orderId,
+      },
+    }
+    const resp = await axios.put('/orders/oneTime/cancel', config)
+    return resp
+  } catch (error) {
+    return error.response
+  }
+}
