f07ccc58d8c55d24bff6f9cf0319083bd7f60847
Rayyanirfan2 <89404776+Rayyanirfan2@users.noreply.github.com>
2023-02-17 23:39:03 +0500

    changes

--- root/Screens/Profile/Freelancer/CompletedJobsScreen.js
+++ root/Screens/Profile/Freelancer/CompletedJobsScreen.js
@@ -10,22 +10,31 @@ import MyText from '../../../Components/Text'
 import Context from '../../../Context/Context'
 import Icon from '@expo/vector-icons/FontAwesome'
 
-import { useNavigation } from '@react-navigation/native'
+import { useIsFocused, useNavigation } from '@react-navigation/native'
 import CustomHeader from '../../../Components/CustomHeader'
 import { getOrderCategoryWise, getOrders } from '../services/orderServices'
 
 import axios from '../../../http/axiosSet'
+import Loader from '../../../Components/Loader'
 
 const CompletedJobsScreen = () => {
   const navigation = useNavigation()
 
   const [orders, setOrders] = useState([])
+
+  const isFocused = useIsFocused()
+
+  const [loading, setLoading] = useState(true)
+
   useEffect(() => {
     fetchOrder()
-  }, [])
+  }, [isFocused])
 
   const fetchOrder = async () => {
+    setLoading(true)
+
     const resp = await getOrderCategoryWise('Completed')
+    setLoading(false)
 
     if (resp.status === 200) {
       setOrders(resp.data.data)
@@ -185,6 +194,10 @@ const CompletedJobsScreen = () => {
       </View>
     </TouchableOpacity>
   )
+
+  if (loading) {
+    return <Loader visible={loading} color='white' indicatorSize='large' />
+  }
 
   return (
     <View style={{ marginTop: 33 }}>
